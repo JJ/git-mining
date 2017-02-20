08618a9d91f14e7038c869bd0619ab8be7c13c2b
JJ Merelo <jjmerelo@gmail.com>
2017-02-20 17:06:48 +0100

    Rearranging files

--- network-evolution-animation.R
+++ network-evolution-animation.R
@@ -2,7 +2,6 @@
                                         #load libraries
 # from http://estebanmoro.org/2015/12/temporal-networks-with-r-and-igraph-updated/
 require(igraph)
-require("RColorBrewer")
  
 #load the edges with time stamp
 #there are three columns in edges: id1,id2,time
@@ -10,11 +9,6 @@ edges <- read.table("vue-edges.csv",header=T)
  
 #generate the full graph
 g <- graph.data.frame(edges,directed=F)
- 
-#generate a cool palette for the graph (darker colors = older nodes)
-YlOrBr.pal <- colorRampPalette(brewer.pal(8,"YlOrRd"))
-#colors for the nodes are chosen from the very beginning
-V(g)$color <- rev(YlOrBr.pal(vcount(g)))[as.numeric(V(g)$name)]
  
 #time in the edges goes from 1 to 300. We kick off at time 3
 ti <- 3
@@ -25,7 +19,6 @@ layout.old <- norm_coords(layout.graphopt(gt), xmin = -1, xmax = 1, ymin = -1, y
  
 #total time of the dynamics
 total_time <- max(E(g)$time)
-dt <- 0.1
 #Output for each frame will be a png with HD size 1600x900 <img draggable="false" class="emoji" alt="🙂" src="https://s.w.org/images/core/emoji/2.2.1/svg/1f642.svg">
 png(file="animation/vue%03d.png", width=1600,height=900)
 #Time loop starts
@@ -35,7 +28,7 @@ for(time in seq(3,total_time)){
   #with the new graph, we update the layout a little bit
     layout.new <- layout_with_fr(gt,coords=layout.old,niter=10,start.temp=0.05,grid="nogrid")
                                         #plot the new graph
-    plot(gt,layout=layout.new,vertex.label="",vertex.size=1+2*log(degree(gt)),vertex.frame.color=V(g)$color,edge.width=1.5,asp=9/16,margin=-0.15)
+    plot(gt,layout=layout.new,vertex.label="",vertex.size=1+2*log(degree(gt)),edge.width=1.5,asp=9/16,margin=-0.15)
                                         #use the new layout in the next round
     layout.old <- layout.new
 }
