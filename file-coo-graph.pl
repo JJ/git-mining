fafe380b19ce669f17e1e771064ad35fde042f2d
JJ Merelo <jjmerelo@gmail.com>
2017-02-21 08:22:15 +0100

    Adds extension graph extension
    And analysis for webpack. Takes some time. It pretty much confirms
    that the most important relationships are between files of the same
    extension, but there are strong relationship with text files.

--- file-coo-graph.pl
+++ file-coo-graph.pl
@@ -4,6 +4,10 @@
 
 get-author-files.pl - download social graph stats from a github repo
 
+=head1 SYNOPSIS
+
+Creates a series of network files from a repository by extracting information from commits. 
+
 =head1 SEE ALSO
 
 Check out L<http://github.com/JJ/literaturame>, for some results, generated reports, R files for analyzing them, and so on. I would be grateful if you added your results to the L<https://github.com/JJ/literaturame/blob/master/data.md> file via pull request.
@@ -27,6 +31,7 @@ my @these_revs = `cd $dir; git rev-list --all`;
 
 my $commit_net = SNA::Network->new();
 my %commit_nodes;
+my %extensions;
 my $time = 1;
 say "id0 id1 time";
 for my $commit ( reverse @these_revs ) {
@@ -35,16 +40,19 @@ for my $commit ( reverse @these_revs ) {
   my @files = ($commit_info =~ /\+\+\+\s+b\/(.+)/g);
   for (my $i = 0; $i <= $#files; $i++ ) {
     my $f = $files[$i];
+    my $this_ext = extension( $f );
     if ( !$commit_nodes{$f} ) {
       my $this_node = $commit_net->create_node( name => "\"$f\"" );
       $commit_nodes{$f} = $this_node;
     }
     if ( $i < $#files ) {
       for ( my $j = $i+1; $j<=$#files; $j++ ) {
+	my $that_ext = extension( $files[$j]);
 	if ( !$commit_nodes{$files[$j]} ) {
 	  my $this_node = $commit_net->create_node( name => "\"$files[$j]\"" );
 	  $commit_nodes{$files[$j]} = $this_node;
 	}
+	create_update_edge_extensions( \%extensions, $this_ext, $that_ext );
 	$commit_net->create_edge( source_index => $commit_nodes{$f}->{'index'},
 				  target_index => $commit_nodes{$files[$j]}->{'index'});
 	say "$f $files[$j] $time";
@@ -54,6 +62,7 @@ for my $commit ( reverse @these_revs ) {
   $time++;
 }
 
+write_extension_file( $repo_name, \%extensions);
 write_correct_file( $commit_net, "commit", $repo_name  );
 
 # Hack for avoiding errors in Pajek file and writing two files.
@@ -66,4 +75,47 @@ sub write_correct_file {
     $net_file =~ s/\*Arcs/*arcs/;
     write_file("$name-$repo_name.net", $net_file);
     $net->save_to_gdf(filename => "$name-$repo_name.gdf",  node_fields => ['betweenness','authority','hub'] );
+  }
+
+# Work with extensions
+sub write_extension_file {
+  my $repo = shift;
+  my $hash = shift;;
+  my %nodes;
+  my $extension_net = SNA::Network->new();
+  for my $k ( keys %$hash ) {
+    if (!$nodes{$k} ) {
+      $nodes{$k} = $extension_net->create_node( name => "\"$k\"" );
+    }
+    for my $j ( keys %{$hash->{$k}} ) {
+      if (!$nodes{$j} ) {
+	$nodes{$j} = $extension_net->create_node( name => "\"$j\"" );
+      }
+      $extension_net->create_edge( source_index => $nodes{$k}->{'index'},
+				   target_index => $nodes{$j}->{'index'},
+				   weight => $hash->{$k}{$j});
+    }
+  }
+  write_correct_file( $extension_net, "extensions", $repo );
+}
+
+#Extract extension
+sub extension {
+  my $f = shift;
+  my ($this_ext) = ( $f =~ /\.(\w+)$/ );
+  return $this_ext || "Ø";
+}
+
+#Create or add to edge
+sub create_update_edge_extensions {
+  my $network = shift;
+  my $this_node = shift;
+  my $that_node = shift;
+  if ( $this_node gt $that_node ) { #lexicographical order
+    my $temp = $this_node;
+    $this_node = $that_node;
+    $that_node = $temp;
+  }
+  $network->{$this_node}{$that_node}++;
+  
 }
