b0be3742db74552124d49596597bf51bf3d7450f
JJ Merelo <jjmerelo@gmail.com>
2017-02-28 19:31:27 +0100

    Using other file

--- sankey.R
+++ sankey.R
@@ -3,8 +3,9 @@ library(networkD3)
  
 #load the edges with time stamp
 #there are three columns in edges: id1,id2,time
-fcmn <- fromJSON("data/extensions-pandoc.json")
+fcmn <- fromJSON("data/extensions-rakudo.json")
 
-sankeyNetwork(Links = fcmn$links, Nodes = fcmn$nodes, Source = "source",
+
+sankeyNetwork(Links = fcmn$links[fcmn$links['weight'] > 5,], Nodes = fcmn$nodes, Source = "source",
               Target = "target", Value = "weight", NodeID = "name",
               fontSize = 12, nodeWidth = 30, sinksRight=T)
